e1566fc427c7e8073cc308489c9e7ed208729387
MikiPoznan <55251314+MikiPoznan@users.noreply.github.com>
2021-11-21 15:48:37 +0100

    Add files via upload
    ✔️Added Commands:
    - 🔥!kick (Kicks user from server)
    - 🔥 !hi (Test command)

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from math import exp
 import socket
 import threading
 from random import randint
+import string
+import random
 
 HOST = '127.0.0.1'
 PORT = 2137
@@ -9,6 +11,27 @@ PORT = 2137
 users = {}
 users_con = {}
 socks = []
+admins = []
+
+#def id_generator(size, chars=string.ascii_lowercase + string.digits):
+#    return ''.join(random.choice(chars) for _ in range(size))
+
+def handle_command(msg, conn, addr):
+    command_list = list(msg.split(' '))
+    if command_list[0].strip() == "!hi":
+        txt = bytes(f"[COMMAND] Hi {users[addr]} \n",encoding='utf8')
+        conn.sendall(txt)
+    elif command_list[0].strip() == "!kick":
+        if command_list[1].strip() in users_con:
+            users_con[command_list[1].strip()].close()
+        else:
+            txt = bytes(f"[COMMAND] Couldn't find user {command_list[1].strip()} \n",encoding='utf8')
+            conn.sendall(txt)
+    else:
+        txt = bytes(f"Couldn't find command: {command_list[0].rsplit()} \n",encoding='utf8')
+        conn.sendall(txt)
+
+#admin_pass = id_generator(6)
 
 def sendtoall(msg):
     try:
@@ -39,8 +62,13 @@ def handle_client(conn, addr):
             if not data:
                 break
             msg_decoded = data.decode("utf-8")
-            msg = f"{users[addr]} : {msg_decoded}"
-            sendtoall(msg)
+            if msg_decoded.startswith("!"):
+                thread = threading.Thread(target=handle_command, args=(msg_decoded,conn, addr))
+                thread.start()
+            else:
+                msg = f"{users[addr]} : {msg_decoded}"
+                print(msg)
+                sendtoall(msg)
         except:
             connected = False
             socks.remove(conn)
@@ -52,6 +80,7 @@ def handle_client(conn, addr):
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     print(f"[INFO] Started server at -> {HOST}:{PORT}")
+#    print(f"[INFO] Created admin password -> {admin_pass}")
     s.listen()
     while True:
         conn, addr = s.accept()
